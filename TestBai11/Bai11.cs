bea7dff1a8ef8e57c8d199b6095ca80b8e01a313
nguyenthuan202 <nguyenthuan020010@gmail.com>
2024-09-29 11:36:03 +0700

    commit TestBai11

---
+++ TestBai11/Bai11.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MethodLibrary;
+
+namespace TestBai11
+{
+    [TestClass]
+    public class Bai11
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\Data11.csv", "data11#csv", DataAccessMethod.Sequential)]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            String s1 = TestContext.DataRow["s1"].ToString();
+            String s2 = TestContext.DataRow["s2"].ToString();
+            String s3 = TestContext.DataRow["s3"].ToString();
+            String expected = TestContext.DataRow["rst"].ToString();
+
+            String result = methodLibrary.ThayThe(s1, s2, s3);
+
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ TestBai11/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBai11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBai11")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("610863b9-86a3-4fe8-9ff2-28f979d46857")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
