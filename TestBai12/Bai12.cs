3aeb7efcc28450c5b5239317c44cf3f3cf9ccfbb
nguyenthuan202 <nguyenthuan020010@gmail.com>
2024-09-29 23:57:53 +0700

    commit bai 12

---
+++ TestBai12/Bai12.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MethodLibrary;
+
+namespace TestBai12
+{
+    [TestClass]
+    public class Bai12
+    {
+        [TestMethod] // Test case 1
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            int[] a = { 2, 3, 4, 5 };
+
+            int ex = 5;
+
+            int result = methodLibrary.Largest(a);
+
+            Assert.AreEqual(ex, result);
+        }
+
+        [TestMethod] // Test case 3
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            int[] a = new int[0];
+
+            int ex = int.MaxValue;
+
+            int result = methodLibrary.Largest(a);
+
+            Assert.AreEqual(ex, result);
+        }
+
+        [TestMethod] // Test case 4
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            int[] a = { 2, 3, 4, 2147483647 };
+            
+            int ex = int.MaxValue;
+
+            int result = methodLibrary.Largest(a);
+
+            Assert.AreEqual(ex, result);
+        }
+
+        [TestMethod] // Test case 5
+        public void TestMethod4()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            int[] a = { -2147483647, -2147483647 };
+;
+
+            int ex = int.MinValue;
+
+            int result = methodLibrary.Largest(a);
+
+            Assert.AreEqual(ex, result);
+        }
+
+    }
+}
---
+++ TestBai12/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBai12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBai12")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fbc8b89a-ba89-4942-8968-e267b7d8c7db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
